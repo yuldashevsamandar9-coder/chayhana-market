9d9afc39a58646fdc57d4f94401c5efd31254df5
Sam <yuldashevsamandar9@gmail.com>
2026-07-19 20:43:14 +0900

    feat: Connect mongoDb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {}) // bush obyekt keyinchalik malumot qushish uchun.
+  .then((data) => {
+    console.log(`Mongodb suceccfully`);
+    const PORT = process.env.PORT ?? 3007;
+  })
+
+  .catch((err) => {
+    console.log("Error wrong try agein:", err);
+  });
