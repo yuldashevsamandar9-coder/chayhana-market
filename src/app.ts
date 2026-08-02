445c9afa120082e595bcb29a8ba388c1c1df5f03
Sam <yuldashevsamandar9@gmail.com>
2026-08-02 23:33:07 +0900

    feat: intgrate middelware sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,15 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL as string,
+  collection: "sessions",
+});
+
 /**  1 - ENTRANSE  **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public"))); // Middelwere
@@ -12,6 +21,18 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /**  2 - SESSIONS **/
+
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 hafta davomida sessiya saqlanadi, keyin esa avtomatik o'chadi
+    },
+    store: store,
+    resave: true, // trueda belgilangan vaqt orasida qayta kirsak saytga yana vaqti yangilanadi
+    saveUninitialized: true,
+  }),
+);
 
 /**  3 - VIEWS **/
 app.set("views", path.join(__dirname, "views"));
